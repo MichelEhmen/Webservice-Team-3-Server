a5486636366bb0791994364bd561fd20febe49ce
BobaFett1 <michelehmen@hotmail.de>
2016-06-09 19:52:47 +0200

    Datenbank URL wurde angepassst

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/messenger';
+var connectionString = process.env.DATABASE_URL || 'postgres://hwevjgxxtdssqz:eVImWmNFV1AC-xpbWrj7nkCFQO@ec2-54-221-226-72.compute-1.amazonaws.com:5432/degmveu9ep94bm';
 
 module.exports = connectionString;
