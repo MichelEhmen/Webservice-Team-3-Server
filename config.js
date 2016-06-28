6a68c30cf9d0792a041c795727e89a1f2e079240
BobaFett1 <michelehmen@hotmail.de>
2016-06-28 13:49:19 +0200

    neue Datenbankverbindung

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://sqawnktvlaijwk:TY4z6xT_fxdwwhkXnpMOBTcpR_@ec2-54-243-201-116.compute-1.amazonaws.com:5432/d77240emvm8oib';
+var connectionString = process.env.DATABASE_URL || 'postgres://unxzhqlfweankm:Lb1hzxUNjJ1oVkyiiVs7jxjk0V@ec2-23-21-50-120.compute-1.amazonaws.com:5432/daohp952t37746';
 
 module.exports = connectionString;
